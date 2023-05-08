594b7b3e6b3056a35e860f3b8217c0d4678b7fd5
apisavengers <132921545+apisavengers@users.noreply.github.com>
2023-05-08 07:50:08 -0700

    Add S3 backend configuration

--- provider.tf
+++ provider.tf
@@ -1,5 +1,12 @@
 provider "aws" {
   region  = "ap-south-1" # Don't change the region
 }
-
 # Add your S3 backend configuration here
+terraform {
+  backend "s3" {
+    bucket = "3.devops.candidate.exam"
+    key    = "andy.pham"
+    region = "ap-south-1"
+    skip_credentials_validation = true
+  }
+}
