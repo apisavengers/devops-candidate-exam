93d64335c7c44dc3544d009d19db8ef1aa324950
apisavengers <132921545+apisavengers@users.noreply.github.com>
2023-05-09 10:30:58 -0700

    Update availability zone to ap-south-1c for creating subnet

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_route_table" "ap_route_table" {
 resource "aws_subnet" "ap_subnet" {
     vpc_id = "${data.aws_vpc.vpc.id}"
     cidr_block = "10.0.180.0/24"
-    availability_zone = "ap-south-1"
+    availability_zone = "ap-south-1c"
     tags = {
         Name = "ap_subnet"
     }
